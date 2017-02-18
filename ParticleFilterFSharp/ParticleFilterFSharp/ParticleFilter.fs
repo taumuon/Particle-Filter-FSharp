32aed1d21e9e212dfed203d6ff3764af04e7f3a7
Gary Evans <Gary Evans>
2017-02-18 20:18:19 +0000

    initial commit

---
+++ ParticleFilterFSharp/ParticleFilterFSharp/ParticleFilter.fs
@@ -0,0 +1,93 @@
+﻿namespace ParticleFilter
+
+open System;
+
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols;
+
+open MathNet.Numerics.Random;
+open MathNet.Numerics.Distributions;
+
+module ParticleFilter =
+    
+    type StateVector = { x : float<m> ; y : float<m> ; orientation : float }
+
+    type ControlVector = { move : float<m> ; rotate : float }
+
+    type World = { robot : StateVector; particleStates : StateVector list }
+
+    type Noise = { sense : float ; forward : float ; turn : float }
+
+    type UpdateParams = { control : ControlVector ; markers : seq<float<m> * float<m>> ; noise : Noise }
+
+    let create_random_particle (worldsize:float) (random:System.Random) =
+        let x = random.NextDouble() * worldsize
+        let y = random.NextDouble() * worldsize
+        let orientation = random.NextDouble() * 2.0 * System.Math.PI;
+        { x = x * 1.0<m>; y = y * 1.0<m>; orientation = orientation }
+
+    let create_particles worldsize count (random:System.Random) =
+        let create_random_particle' = fun () -> create_random_particle worldsize random
+        List.init count (fun _ -> create_random_particle'())
+
+    let gauss mean sigma random = Normal.Sample(random, mean, sigma)
+
+    let distance_from_landmark robotState (landmark : (float<m> * float<m>)) = 
+        let (x, y) = landmark;
+        sqrt(((float(robotState.x - x)) ** 2.0) + (float(robotState.y - y)) ** 2.0)
+
+    let sense senseNoise (landmarks:seq<float<m> * float<m>>) random (robotState:StateVector) =
+        landmarks |> Seq.map(fun landmark ->
+                                 let dist = distance_from_landmark robotState landmark
+                                 dist + (gauss 0.0 senseNoise random))
+                  |> Seq.toArray
+
+    let gaussian mu sigma x =
+        exp(- ((mu - x) ** 2.0) / (sigma ** 2.0) / 2.0) / sqrt(2.0 * Math.PI * (sigma ** 2.0))
+
+    let measurement_prob (measurement:float[]) (landmarks:seq<float<m> * float<m>>) sense_noise state =
+        landmarks |> Seq.zip measurement
+                  |> Seq.fold (fun current_prob (measurement, landmark) ->
+                                   let dist = distance_from_landmark state landmark
+                                   current_prob * gaussian dist sense_noise measurement) 1.0
+
+    // also called stochastic universal sampling?
+    // search online for fast perfect weighted resampling algorithm paper
+    let inline weighted_resample (random:System.Random) (input: array<(float * ^a)>) =
+        let inputLength = input.Length
+        let mw = input |> Seq.maxBy (fun (weight, value) -> weight) |> fst
+        let mutable index = random.Next(0, inputLength)
+        let mutable beta = 0.0
+
+        seq {
+            for i = 0 to inputLength do
+                beta <- beta + random.NextDouble() * 2.0 * mw
+                while beta > fst input.[index] do
+                    beta <- beta - fst input.[index]
+                    index <- (index + 1) % inputLength
+                yield snd input.[index]
+            }
+        |> Seq.toArray
+
+    let update_state forward_noise turn_noise random control state =
+        let orientation = state.orientation + control.rotate + (random |> gauss 0.0 turn_noise);
+        let wrappedOrientation = if orientation >= 2.0 * Math.PI then orientation - (2.0 * Math.PI) else orientation
+
+        let dist = control.move + ((random |> gauss 0.0 forward_noise) * 1.0<m>)
+        let x = state.x + (cos(wrappedOrientation) * dist)
+        let y = state.y + (sin(wrappedOrientation) * dist)
+        let wrappedX = if x < 0.0<m> then (x + 100.0<m>) else if x > 100.0<m> then (x - 100.0<m>) else x
+        let wrappedY = if y < 0.0<m> then (y + 100.0<m>) else if y > 100.0<m> then (y - 100.0<m>) else y
+        { x = wrappedX; y = wrappedY; orientation = wrappedOrientation }
+
+    let update (random : System.Random) (updateParams : UpdateParams) {robot = r;  particleStates = p } =
+        let updateParticleState = update_state updateParams.noise.forward updateParams.noise.turn random updateParams.control
+
+        let new_r = r |> update_state 0.0 0.0 random updateParams.control
+        let measurement = sense updateParams.noise.sense updateParams.markers random new_r
+
+        let new_p = p |> List.map (fun p -> p |> updateParticleState)
+                      |> List.map (fun p -> (measurement_prob measurement updateParams.markers updateParams.noise.sense p, p))
+                      |> List.toArray 
+                      |> (weighted_resample random)
+
+        { robot = new_r ; particleStates = new_p |> Array.toList }
---
+++ ParticleFilterFSharp/ParticleFilterFSharp/Program.fs
@@ -0,0 +1,81 @@
+﻿open ParticleFilter.ParticleFilter; 
+
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+
+open FSharp.Charting
+open FSharp.Charting.ChartTypes
+
+open System
+open System.Drawing
+open System.Windows.Forms
+open System.Reactive
+open System.Reactive.Linq
+
+open EventEx;
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv = 
+    Application.EnableVisualStyles()
+    Application.SetCompatibleTextRenderingDefault false
+
+    // Create a form to host chart instead of showing chart directly,
+    //  so synchronisation context is created for Rx
+    let hostForm = new Form(Visible = true, TopMost = true, Width = 500, Height = 500)
+
+    let random = new System.Random();
+    let particles = create_particles 100.0 1000 random
+
+    let robot = { x = 50.0<m>; y = 50.0<m>; orientation = 0.0 }
+    let initialWorld =  { robot = robot; particleStates = particles }
+
+    let control = { move = 1.0<m>; rotate = 0.05 }
+    let markers = [|(20.0<m>, 20.0<m>); (80.0<m>, 80.0<m>); (20.0<m>, 80.0<m>); (80.0<m>, 20.0<m>)|]
+    let noise = { sense = 5.0; forward = 0.05; turn = 0.05 }
+    let updateParams = { control = control; markers = markers; noise = noise }
+
+    let update' = update random updateParams
+
+    let worldUpdate = Observable.Interval(TimeSpan.FromMilliseconds(500.0))
+                                .ObserveOn(WindowsFormsSynchronizationContext.Current)
+                                |> Observable.scan (fun currentWorld t -> currentWorld |> update') initialWorld
+
+    // Observable.Interval is a cold observable, warm up at this point to ensure that both charts are
+    //  displaying the same simulation.
+    let sharedUpdate = worldUpdate.Publish().RefCount()
+
+    let chartUpdate = (Observable.Return initialWorld).Concat sharedUpdate
+
+    let particlesUpdate = chartUpdate |> Observable.map (fun w -> w.particleStates |> Seq.map(fun p -> (p.x, p.y)))
+    let robotUpdate = chartUpdate |> Observable.map(fun w -> [|w.robot|] |> Seq.map(fun p -> (p.x, p.y) ))
+
+    let particlesChart = LiveChart.Point(particlesUpdate)  
+    let robotActualPositionChart = LiveChart.Point(robotUpdate).WithMarkers(System.Drawing.Color.Red)
+
+    let chart = Chart.Combine([ particlesChart
+                                robotActualPositionChart ])
+                     .WithXAxis(Min = 0.0, Max = 100.0)
+                     .WithYAxis(Min = 0.0, Max = 100.0)
+
+    let chartControl = new ChartControl(chart)
+    chartControl.Dock <- DockStyle.Fill
+    hostForm.Controls.Add(chartControl)
+
+// This can be uncommented to render out each frame to a file
+// generate animated gif using imagemagick: magick -delay 20 -loop 0 *.png pf.gif
+//    let mutable image_number = 0;
+//    chartUpdate.Take(30)
+//               .Subscribe(fun world ->
+//                                let staticParticles = world.particleStates |> Seq.map(fun p -> (p.x, p.y))
+//                                let staticRobot = [|world.robot|] |> Seq.map(fun p -> (p.x, p.y) )
+//                                let staticChart = Chart.Combine([ Chart.Point(staticParticles)
+//                                                                  Chart.Point(staticRobot).WithMarkers(System.Drawing.Color.Red) ])
+//                                                       .WithXAxis(Min = 0.0, Max = 100.0)
+//                                                       .WithYAxis(Min = 0.0, Max = 100.0)
+//                                staticChart.ShowChart() |> ignore
+//                                staticChart.SaveChartAs("e:\\temp\\pf\\" + image_number.ToString("000") + ".png", ChartImageFormat.Png)
+//                                image_number <- image_number + 1
+//                                ) |> ignore
+
+    System.Windows.Forms.Application.Run(hostForm);
+    0
